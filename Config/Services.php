17c076f035b15a6fa73c6945294fe4dc0fee4ce8
Codenom Web Developer <47415105+codenomdev@users.noreply.github.com>
2020-09-26 16:36:55 +0700

    Initial Commit
    Initial Commit

---
+++ Config/Services.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Codenom\Framework\Config;
+
+class Services
+{
+    public function test()
+    {
+        return 'test vendor';
+    }
+    public function test1()
+    {
+        return 'test vendor';
+    }
+}
